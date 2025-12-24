b3883c640eb180db834764538dc92931af65febd
piyushsatti <piyushsatti@gmail.com>
2025-12-24 14:55:34 -0500

    ep03 completed

--- App.js
+++ App.js
@@ -8,7 +8,23 @@ const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
 const jsxHeading = (
 <h1 className="head">Namaste React using JSX</h1>);
 
+const Title = () => {
+  return (
+    <h1>Namaste React Functional Component</h1>
+  );
+}
+
+// React Functional Component
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h2>This is a nested component?</h2>
+    </div>
+  );
+}
+
 // Adding component to DOM
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);

219512f76b90beeb6edfd8bb948422429903dbf4
piyushsatti <piyushsatti@gmail.com>
2025-12-27 22:23:43 -0500

    ep-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,53 @@
+import { useEffect, useState } from "react";
+
 import RestrauntCard from "./Restraunt";
 import resData from "../utils/constants";
 
 const Body = () => {
+
+  const [restraunts, setRestraunts] = useState(resData);
+
+  useEffect(() => {
+    const a = async () => {
+      const raw_data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9966135&lng=77.5920581&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+      );
+      const data = await raw_data.json();
+      console.log(data.data.cards.slice(3));
+      setRestraunts(data.data.cards.slice(3));
+    }
+    a();
+  }, []);
+
   return (
     <div className="body">
-        <div className="Search">
-          Search
-        </div>
-        <div className="res-container">
-          { 
-            resData.map((restraunt) => {
-                return <RestrauntCard key={restraunt.card.card.info.id} resData={restraunt}/>
+      <div className="body-top">
+          <div className="filter">
+            <button className="filter-btn" onClick={
+              () => {
+                setRestraunts(resData.filter(
+                  (restraunt) => {
+                    return restraunt.card.card.info.avgRating > 4.3;
+                  }
+                ))
               }
-            )
-          }
-        </div>
+            }
+            >
+              Top Rated Restraunts
+            </button>
+          </div>
+          <div className="Search">
+            Search
+          </div>
+      </div>
+      <div className="res-container">
+        { 
+          restraunts.map((restraunt) => {
+              return <RestrauntCard key={restraunt.card.card.info.id} resData={restraunt}/>
+            }
+          )
+        }
+      </div>
     </div>
   );
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-
+import React, { useState } from "react";
 
 const RES_DATA = [
   {
